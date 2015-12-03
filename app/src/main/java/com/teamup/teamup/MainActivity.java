fc7f7c0542eca2d2e246fc2c6ae6f3cd7723e075
Rushabh Shah <Rushabh@dyn-160-39-149-251.dyn.columbia.edu>
2015-12-02 22:22:39 -0500

    Search Bar

--- app/src/main/java/com/teamup/teamup/MainActivity.java
+++ app/src/main/java/com/teamup/teamup/MainActivity.java
@@ -49,4 +49,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void search(View view) {
+        System.out.println("Search for events");
+    }
 }
